b99af37fcbafc8c89f3ad9d9518648988894d7ad
Billy丶斌 <641833868@qq.com>
2019-06-21 10:17:37 +0800

    分类话题列表

--- routes/web.php
+++ routes/web.php
@@ -25,10 +25,6 @@ Route::get('email/resend', 'Auth\VerificationController@resend')->name('verifica
 //个人页面
 Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
 
-
-
-
-
-
-
+//话题分类相关
 Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
