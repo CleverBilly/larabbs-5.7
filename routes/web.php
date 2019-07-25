bb301c2cd72f4a04dd5d1be305e961cebf3d0f64
BillyLiu <641833868@qq.com>
2019-07-25 02:11:49 +0000

    消息通知列表

--- routes/web.php
+++ routes/web.php
@@ -34,3 +34,4 @@ Route::get('topics/{topic}/{slug?}', 'TopicsController@show')->name('topics.show
 
 //帖子回复相关
 Route::resource('replies', 'RepliesController', ['only' => ['store', 'destroy']]);
+Route::resource('notifications', 'NotificationsController', ['only' => ['index']]);
