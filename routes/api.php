5415545b24c317e5effce4653231f06c0258ff99
Billy丶斌 <641833868@qq.com>
2019-07-29 16:46:50 +0800

    add easysms

--- routes/api.php
+++ routes/api.php
@@ -13,16 +13,6 @@ use Illuminate\Http\Request;
 |
 */
 
-$api = app('Dingo\Api\Routing\Router');
-
-$api->version('v1', function($api) {
-    $api->get('version', function() {
-        return response('this is version v1');
-    });
-});
-
-$api->version('v2', function($api) {
-    $api->get('version', function() {
-        return response('this is version v2');
-    });
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
 });
