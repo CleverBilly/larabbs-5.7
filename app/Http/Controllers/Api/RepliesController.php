54e91dae058a8ef3681cf70751210d14f1708a9b
Billy丶斌 <641833868@qq.com>
2019-08-16 12:02:06 +0800

    replies destroy

--- app/Http/Controllers/Api/RepliesController.php
+++ app/Http/Controllers/Api/RepliesController.php
@@ -15,8 +15,19 @@ class RepliesController extends Controller
         $reply->topic_id = $topic->id;
         $reply->user_id = $this->user()->id;
         $reply->save();
-        dd(11);
         return $this->response->item($reply, new ReplyTransformer())
             ->setStatusCode(201);
+    }
+
+    public function destroy(Topic $topic, Reply $reply)
+    {
+        if ($reply->topic_id != $topic->id) {
+            return $this->response->errorBadRequest();
+        }
+
+        $this->authorize('destroy', $reply);
+        $reply->delete();
+
+        return $this->response->noContent();
     }
 }
