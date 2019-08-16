d96f9a327cc3cf4504167756cc71102e7d8eada6
Billy丶斌 <641833868@qq.com>
2019-08-16 09:20:27 +0800

    user update

--- app/Http/Controllers/Api/UsersController.php
+++ app/Http/Controllers/Api/UsersController.php
@@ -2,10 +2,11 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Models\User;
-use Illuminate\Http\Request;
-use App\Transformers\UserTransformer;
 use App\Http\Requests\Api\UserRequest;
+use App\Models\Image;
+use App\Models\User;
+use App\Transformers\UserTransformer;
+use Illuminate\Http\Request;
 
 class UsersController extends Controller
 {
@@ -44,5 +45,21 @@ class UsersController extends Controller
     public function me()
     {
         return $this->response->item($this->user(), new UserTransformer());
+    }
+
+    public function update(UserRequest $request)
+    {
+        $user = $this->user();
+
+        $attributes = $request->only(['name', 'email', 'introduction']);
+
+        if ($request->avatar_image_id) {
+            $image = Image::find($request->avatar_image_id);
+
+            $attributes['avatar'] = $image->path;
+        }
+        $user->update($attributes);
+
+        return $this->response->item($user, new UserTransformer());
     }
 }
