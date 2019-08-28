fab436fca650d80b55a88e81429b3440c102cda5
Billy丶斌 <641833868@qq.com>
2019-08-28 17:58:47 +0800

    change jwt to passport

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,10 +9,11 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Contracts\Auth\MustVerifyEmail as MustVerifyEmailContract;
 use Spatie\Permission\Traits\HasRoles;
 use Auth;
-
+use Laravel\Passport\HasApiTokens;
 
 class User extends Authenticatable implements JWTSubject
 {
+    use HasApiTokens;
     use Traits\LastActivedAtHelper;
     use Traits\ActiveUserHelper;
     use MustVerifyEmailTrait;
@@ -101,5 +102,14 @@ class User extends Authenticatable implements JWTSubject
     public function getJWTCustomClaims()
     {
         return [];
+    }
+
+    public function findForPassport($username)
+    {
+        filter_var($username, FILTER_VALIDATE_EMAIL) ?
+          $credentials['email'] = $username :
+          $credentials['phone'] = $username;
+
+        return self::where($credentials)->first();
     }
 }
